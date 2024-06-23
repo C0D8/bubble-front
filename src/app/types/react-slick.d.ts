de039287e4594f54360eaacef3d1dda6beae8c4a
c0d8 <matheusaguiar362@gmail.com>
2024-06-22 22:10:00 -0300

    slick

---
+++ src/app/types/react-slick.d.ts
@@ -0,0 +1,19 @@
+declare module 'react-slick' {
+    import * as React from 'react';
+  
+    interface Settings {
+      dots?: boolean;
+      infinite?: boolean;
+      speed?: number;
+      slidesToShow?: number;
+      slidesToScroll?: number;
+      arrows?: boolean;
+      autoplay?: boolean;
+      autoplaySpeed?: number;
+      pauseOnHover?: boolean;
+      [key: string]: any;
+    }
+  
+    export default class Slider extends React.Component<Settings> {}
+  }
+  
