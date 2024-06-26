6c285c5ea5f686350953541062deb7184086c977
c0d8 <matheusaguiar362@gmail.com>
2024-06-26 16:59:49 -0300

    face trakcing change animation

--- src/app/tracking/page.tsx
+++ src/app/tracking/page.tsx
@@ -19,8 +19,10 @@ export default function VideoTracking() {
   const [names, setNames] = useState<string[]>([]);
   const namesRef = useRef<string[]>([]);
   const [detections, setDetections] = useState<faceapi.FaceDetection[]>([]);
+  const imageRef = useRef<HTMLImageElement>(new Image());
+  // const imageRef = useRef<HTMLImageElement | null>(null);
 
-  async function startVideo() {
+  const startVideo = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } } });
       if (videoRef.current) {
@@ -38,7 +40,7 @@ export default function VideoTracking() {
         alert('Failed to access camera. Please ensure the page is served via HTTPS and camera permissions are granted.');
       }
     }
-  }
+  };
 
   useEffect(() => {
     startVideo();
@@ -49,60 +51,68 @@ export default function VideoTracking() {
       const MODEL_URL = '/models';
       await faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL);
     };
-
     loadModels();
+
+    imageRef.current.src = './img/insta.png';
   }, []);
 
   useEffect(() => {
     const handleVideoPlay = async () => {
       const canvas = overlayRef.current;
       const video = videoRef.current;
-
-      if (!canvas || !video) {
-        return;
-      }
-
+    
+      if (!canvas || !video) return;
+    
       const displaySize = { width: video.videoWidth, height: video.videoHeight };
       faceapi.matchDimensions(canvas, displaySize);
-
-      setInterval(async () => {
+    
+      const drawDetections = async () => {
         const detections = await faceapi.detectAllFaces(video, new faceapi.SsdMobilenetv1Options());
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
-        setDetections(detections);
+        setDetections(resizedDetections);
+    
         const context = canvas.getContext('2d');
         if (context) {
           context.clearRect(0, 0, canvas.width, canvas.height);
           faceapi.draw.drawDetections(canvas, resizedDetections);
-
-          context.fillStyle = 'white';
-          context.font = '24px Arial';
-
-          resizedDetections.forEach((detection, index) => {
-            const { x, y } = detection.box;
-            context.fillText(namesRef.current[index] || '', x, y - 10);
+    
+          // Carregar a fonte personalizada 'Inter'
+          const font = new FontFace('Inter', 'url(./fonts/Poppins-ThinItalic.ttf)');
+          font.load().then(loadedFont => {
+            document.fonts.add(loadedFont);
+    
+            // Após a fonte ser carregada, configurar o contexto do canvas
+            context.font = 'italic lighter 30px Inter';
+            context.fillStyle = 'white';
+    
+            resizedDetections.forEach((detection, index) => {
+              const { x, y } = detection.box;
+              context.fillText(namesRef.current[index] || 'unknown', x+detection.box.width, y - 10);
+              context.fillText('3.7', x+detection.box.width, y + 20);
+    
+            });
           });
+
+    
+          requestAnimationFrame(drawDetections); // Chama recursivamente para o próximo quadro
         }
-      }, 300);
+      };
+    
+      requestAnimationFrame(drawDetections);
     };
+    
 
     if (videoRef.current) {
       videoRef.current.addEventListener('play', handleVideoPlay);
     }
-
-  }, []);
+  }, [detections]);
 
   useEffect(() => {
     const sendFrameToServer = async (frame: string) => {
       try {
-        const response = await axios.post('http://localhost:5005/face_coords', { 'image_data' :  frame });
-        response.data.sort((a: any, b: any) => {
-          if (a.x === b.x) {
-            return a.y - b.y;
-          }
-          return a.x - b.x;
-        });
-
-        const extractedNames = response.data.map((item: FaceData)  => item.name);
+        const response = await axios.post('http://localhost:5005/face_coords', { image_data: frame });
+        response.data.sort((a: FaceData, b: FaceData) => (a.x === b.x ? a.y - b.y : a.x - b.x));
+        const extractedNames = response.data.map((item: FaceData) => item.name);
         setNames(extractedNames);
       } catch (error) {
         console.error('Error sending frame to server:', error);
@@ -111,25 +121,21 @@ export default function VideoTracking() {
 
     const captureFrame = (): string | null => {
       const video = videoRef.current;
-      if (!video) {
-        console.log('Video not found');
-        return null;
-      }
+      if (!video) return null;
 
       const canvas = document.createElement('canvas');
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
       const context = canvas.getContext('2d');
       if (!context) return null;
+
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
       return canvas.toDataURL('image/jpeg');
     };
 
     const interval = setInterval(() => {
       const frame = captureFrame();
-      if (frame) {
-        sendFrameToServer(frame);
-      }
+      if (frame) sendFrameToServer(frame);
     }, 3000);
 
     return () => clearInterval(interval);
@@ -143,40 +149,25 @@ export default function VideoTracking() {
     const canvas = overlayRef.current;
     const video = videoRef.current;
     if (!canvas || !video) return;
-  
+
     const rect = canvas.getBoundingClientRect();
-  
-    // Obter as coordenadas do clique relativas ao canvas
     const scaleX = canvas.width / rect.width;
     const scaleY = canvas.height / rect.height;
     const clickX = (event.clientX - rect.left) * scaleX;
     const clickY = (event.clientY - rect.top) * scaleY;
-  
+
     const dims = faceapi.matchDimensions(canvas, video, true);
     const resizedResults = faceapi.resizeResults(detections, dims);
-  
-    resizedResults.sort((a, b) => {
-      if (a.box.x === b.box.x) {
-        return a.box.y - b.box.y;
-      }
-      return a.box.x - b.box.x;
-    });
 
-
-    
+    resizedResults.sort((a, b) => (a.box.x === b.box.x ? a.box.y - b.box.y : a.box.x - b.box.x));
 
     resizedResults.forEach(detection => {
       const { x, y, width, height } = detection.box;
       if (clickX > x && clickX < x + width && clickY > y && clickY < y + height) {
         console.log(`Clicked on face: ${namesRef.current[resizedResults.indexOf(detection)] || 'Unknown'}`);
       }
-
-       
-
     });
   };
-  
-  
 
   return (
     <div style={{ position: 'relative', overflow: 'hidden' }} className="h-screen w-screen">
@@ -184,14 +175,7 @@ export default function VideoTracking() {
       <canvas
         ref={overlayRef}
         onClick={handleCanvasClick}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover'
-        }}
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover' }}
       />
     </div>
   );
